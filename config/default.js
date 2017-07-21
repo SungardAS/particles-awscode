ea98ae9e7819df380c3dd50ff5360aa561f01ab1
Kevin McGrath <kevin.mcgrath@sungardas.com>
2017-07-21 14:17:15 -0400

    add iam user policy

--- config/default.js
+++ config/default.js
@@ -1,17 +1,17 @@
 var gitrev = require('git-rev-sync');
 
 var regions = [
-  'us-east-1',
-  'us-east-2',
-  'us-west-1',
-  'us-west-2',
-  'ap-northeast-1',
-  'ap-southeast-1',
-  'ap-southeast-2',
-  'eu-central-1',
-  'eu-west-1',
-  'eu-west-2',
-  'ca-central-1'
+  'us-east-1'
+  //'us-east-2',
+  //'us-west-1',
+  //'us-west-2',
+  //'ap-northeast-1',
+  //'ap-southeast-1',
+  //'ap-southeast-2',
+  //'eu-central-1',
+  //'eu-west-1',
+  //'eu-west-2',
+  //'ca-central-1'
 ];
 
 var buckets = [];
